9c48c0265329afab1f310753d32b99641ef47248
osamayy <osamayy@gmail.com>
2023-12-02 18:25:29 +0300

    update favicon

---
+++ _nuxt/zakroon.b20ef2c4.js
@@ -0,0 +1,1 @@
+import{c as t,b as e,l as s,o as a}from"./entry.394954b7.js";const _={dir:"rtl"},n=e("div",null,[e("header")],-1),r={class:"container mx-auto"},i={__name:"zakroon",setup(c){return(o,l)=>(a(),t("div",_,[n,e("div",r,[s(o.$slots,"default")])]))}};export{i as default};
