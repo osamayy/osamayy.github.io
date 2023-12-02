dffcff7d2b10229ccda895bbae69308213251fa4
osamayy <osamayy@gmail.com>
2023-12-02 12:10:18 +0300

    show repeat no for azkar

---
+++ _nuxt/zakroon.307a7a30.js
@@ -0,0 +1,1 @@
+import{c as t,b as e,l as s,o as a}from"./entry.a50e53c1.js";const _={dir:"rtl"},n=e("div",null,[e("header")],-1),r={class:"container mx-auto"},i={__name:"zakroon",setup(c){return(o,l)=>(a(),t("div",_,[n,e("div",r,[s(o.$slots,"default")])]))}};export{i as default};
