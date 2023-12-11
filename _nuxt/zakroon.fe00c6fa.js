029815b27dea7de358b4040f78b3914bdd3af159
osamayy <osamayy@gmail.com>
2023-12-11 16:05:00 +0300

    fix dark mode 4

---
+++ _nuxt/zakroon.fe00c6fa.js
@@ -0,0 +1,1 @@
+import{c as t,b as e,q as s,o as a}from"./entry.898f4571.js";const _={dir:"rtl"},n=e("div",null,[e("header")],-1),r={class:"container mx-auto"},i={__name:"zakroon",setup(c){return(o,d)=>(a(),t("div",_,[n,e("div",r,[s(o.$slots,"default")])]))}};export{i as default};
