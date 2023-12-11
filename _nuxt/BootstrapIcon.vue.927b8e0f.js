ad5af779de46bfe1292231f7de2b99c98c318510
osamayy <osamayy@gmail.com>
2023-12-11 17:35:45 +0300

    fix dark mode 5

---
+++ _nuxt/BootstrapIcon.vue.927b8e0f.js
@@ -0,0 +1,1 @@
+import{m as n,o as s,c as e,n as a}from"./entry.89455d6b.js";const m=n({__name:"BootstrapIcon",props:{name:{}},setup(t){return(o,r)=>(s(),e("i",{class:a(["bi",`bi-${o.name}`])},null,2))}});export{m as _};
