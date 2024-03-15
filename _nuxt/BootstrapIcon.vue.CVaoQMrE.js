33c83e7824e56b6fab41ee601ba964e4e75cc887
osamayy <osamayy@gmail.com>
2024-03-15 12:59:33 +0300

    fix accessability2

--- _nuxt/BootstrapIcon.vue.CVaoQMrE.js
+++
@@ -1,1 +0,0 @@
-import{m as o,o as n,c as s,n as a}from"./entry.D5Fm9Nbe.js";const c=o({__name:"BootstrapIcon",props:{name:{}},setup(r){return(e,p)=>(n(),s("i",{class:a("bi bi-"+e.$props.name)},null,2))}});export{c as _};
