fc5e212f5cd1d5a4a187fa256442a15590128433
osamayy <osamayy@gmail.com>
2024-03-15 17:58:30 +0300

    add selected area

--- _nuxt/BootstrapIcon.vue.ChbnEugj.js
+++
@@ -1,1 +0,0 @@
-import{m as o,o as n,c as s,n as a}from"./entry.CeMKajQn.js";const c=o({__name:"BootstrapIcon",props:{name:{}},setup(r){return(e,p)=>(n(),s("i",{class:a("bi bi-"+e.$props.name)},null,2))}});export{c as _};
