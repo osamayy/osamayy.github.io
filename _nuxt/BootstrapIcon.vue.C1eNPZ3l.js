b017bf76d62ce60f09582881a187428ffae85ec8
osamayy <osamayy@gmail.com>
2024-03-23 13:51:13 +0300

    update seo

--- _nuxt/BootstrapIcon.vue.C1eNPZ3l.js
+++
@@ -1,1 +0,0 @@
-import{n as o,o as n,c as s,q as a}from"./entry.D6xhx_nP.js";const c=o({__name:"BootstrapIcon",props:{name:{}},setup(r){return(e,p)=>(n(),s("i",{class:a("bi bi-"+e.$props.name)},null,2))}});export{c as _};
