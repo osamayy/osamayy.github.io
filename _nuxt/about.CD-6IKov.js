ec879784242d13e05e6c7fd8eb0390461c3f0955
osamayy <osamayy@gmail.com>
2024-03-24 15:50:08 +0300

    update osama intro

---
+++ _nuxt/about.CD-6IKov.js
@@ -0,0 +1,1 @@
+import{_ as c,c as s,o as a,p as _,a as n,b as e}from"./entry.BPxBbCGx.js";const d={},t=o=>(_("data-v-65192493"),o=o(),n(),o),p=t(()=>e("h2",null,"About",-1)),r=t(()=>e("p",null,"000000 ",-1)),l=[p,r];function u(o,i){return a(),s("div",null,l)}const f=c(d,[["render",u],["__scopeId","data-v-65192493"]]);export{f as default};
