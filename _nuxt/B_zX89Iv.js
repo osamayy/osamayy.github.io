59b947421d4db7d0c4bab525df423a8f14fa775b
osamayy <osamayy@gmail.com>
2024-06-08 14:52:09 +0300

    update selected bg

---
+++ _nuxt/B_zX89Iv.js
@@ -0,0 +1,1 @@
+import{n as o,o as n,c as s,q as a}from"./CD0mdcX6.js";const c=o({__name:"BootstrapIcon",props:{name:{}},setup(r){return(e,p)=>(n(),s("i",{class:a("bi bi-"+e.$props.name)},null,2))}});export{c as _};
