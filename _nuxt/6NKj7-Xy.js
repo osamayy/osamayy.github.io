d060f31a78c194ba795b424fea2c3113c481101e
osamayy <osamayy@gmail.com>
2025-02-08 20:17:59 +0300

    update sitemap

---
+++ _nuxt/6NKj7-Xy.js
@@ -0,0 +1,1 @@
+import{_ as o,c as a,a as n,o as t}from"./BrXFIG87.js";const c={};function r(s,e){return t(),a("div",null,e[0]||(e[0]=[n("h2",null,"Home page",-1)]))}const d=o(c,[["render",r],["__scopeId","data-v-84f5b5c7"]]);export{d as default};
