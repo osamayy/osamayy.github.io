4ab74c22c3ac3e64b498949df0ee9dcf80d4d6db
osamayy <osamayy@gmail.com>
2026-05-22 14:54:34 +0300

    sitemap index

--- _nuxt/mJVcD1jo.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./C1eAQQW0.js";import{o as n,P as s,w as f,$ as p}from"./CRduRxc_.js";const l={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return n(),s(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{l as default};
