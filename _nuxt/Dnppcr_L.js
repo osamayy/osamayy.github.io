86a4fd6609af17d5e9d7243d264eba3b311b2ae0
osamayy <osamayy@gmail.com>
2026-06-10 07:57:06 +0300

    pent meta

---
+++ _nuxt/Dnppcr_L.js
@@ -0,0 +1,1 @@
+import{_ as t,o as s,c as e,a as n}from"./D0hBu8Bm.js";const o={},a={xmlns:"http://www.w3.org/2000/svg","xmlns:xlink":"http://www.w3.org/1999/xlink"};function r(l,_){return s(),e("svg",a,[..._[0]||(_[0]=[n("path",{d:"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.646-2.646a.5.5 0 0 0-.708-.708l-6 6a.5.5 0 0 0 .708.708z"},null,-1)])])}const F=t(o,[["render",r]]);export{F as default};
